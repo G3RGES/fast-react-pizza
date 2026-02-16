cb46416e1d74bc2a0c570ffc7eb1304c8e72eae3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-16 11:53:35 +0200

    Update Error.jsx

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,7 +1,8 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
 
   return (
     <div>
@@ -12,4 +13,4 @@ function NotFound() {
   );
 }
 
-export default NotFound;
+export default Error;
