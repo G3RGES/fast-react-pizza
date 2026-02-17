dc45f89d5b0bfc0868b1e0f35ff58278f756e4b1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-02-17 09:49:56 +0200

    Update Home.jsx

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h1 className="text-ston-700 text-center text-xl font-semibold">
+      <h1 className="text-center text-xl font-semibold">
         The best pizza.
         <br />
         <span className="text-yellow-500">
