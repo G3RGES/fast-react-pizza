4d27ba6844efe869459b47c0d249332f2b8fb662
G3RGES <georgios.nashaat@gmail.com>
2026-02-21 11:14:49 +0200

    Update Button.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,9 +6,11 @@ function Button({ children, disabled, to, type, onClick }) {
 
   const styles = {
     primary: base + "px-4 py-3 md:px-6 md:py-4",
-    small: base + "py-2 md:px-5 md:py-2.5 text-xs",
+    small: base + "py-2 px-4 md:px-5 md:py-2.5 text-xs",
     secondary:
       "inline-block text-sm rounded-sm px-4 py-2.5 md:px-6 md:py-3.5 bg-transparent border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:text-stone-800 hover:bg-stone-300 focus:text-stone-800 focus:bg-stone-200 focus:outline-none focus:ring focus:ring-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed ",
+    round:
+      "inline-block text-sm bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed rounded-full py-0.5 px-2 md:px-2.5 md:py-1 text-sm",
   };
 
   if (to)
