5b4aa0a7caec05447793c91a5f00d267435c1c9b
G3RGES <georgios.nashaat@gmail.com>
2026-02-21 11:14:51 +0200

    Update UpdateItemQuantity.jsx

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,7 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
 
-function UpdateItemQuantity() {
-  return <div></div>;
+function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button type="round" onClick={dispatch(decreaseItemQuantity(pizzaId))}>
+        -
+      </Button>
+      <Button type="round" onClick={dispatch(increaseItemQuantity(pizzaId))}>
+        +
+      </Button>
+    </div>
+  );
 }
 
 export default UpdateItemQuantity;
